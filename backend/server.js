b882f78e121ca634efa251161aa95da42b2af1da
frankmendez <frankmendezwebdev@gmail.com>
2020-11-21 22:28:56 +0800

    Connecting to the Database

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
-import products from '../backend/data/products.js'
+import connectDB from './config/db.js'
+import products from './data/products.js'
 
 dotenv.config()
+connectDB()
 
 const app = express()
 
