5132545dcbfebb4c048f1fe0b6646b96f7a5de8d
frankmendez <frankmendezwebdev@gmail.com>
2020-11-26 15:18:02 +0800

    Add To Cart Functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
-import { ADD_CART_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM } from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
 	const { data } = await axios.get(`/api/products/${id}`)
 	dispatch({
-		type: ADD_CART_ITEM,
+		type: CART_ADD_ITEM,
 		payload: {
 			product: data._id,
 			name: data.name,
