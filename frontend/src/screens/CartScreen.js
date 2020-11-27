9bb63b5eedf99a88a3f64241094e6c2f10977c47
frankmendez <frankmendezwebdev@gmail.com>
2020-11-27 15:54:15 +0800

    Remove Items From Cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
 	const dispatch = useDispatch()
@@ -19,7 +19,9 @@ const CartScreen = ({ match, location, history }) => {
 		}
 	}, [dispatch, productId, qty])
 
-	const removeFromCartHandler = () => {}
+	const removeFromCartHandler = (id) => {
+		dispatch(removeFromCart(id))
+	}
 
 	const checkoutHandler = () => {
 		history.push('/login?redirect=shipping')
@@ -59,12 +61,7 @@ const CartScreen = ({ match, location, history }) => {
 										</Form.Control>
 									</Col>
 									<Col md={2}>
-										<Button
-											type='button'
-											onClick={() => {
-												removeFromCartHandler(item.product)
-											}}
-										>
+										<Button type='button' onClick={() => removeFromCartHandler(item.product)}>
 											<i className='fas fa-trash'></i>
 										</Button>
 									</Col>
