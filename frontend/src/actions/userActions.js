382122a2f427e8ed035652e300a2613e9e5604db
frankmendez <frankmendezwebdev@gmail.com>
2020-12-03 17:12:15 +0800

    Profile Screen & Get User Details

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -6,6 +6,9 @@ import {
 	USER_REGISTER_REQUEST,
 	USER_REGISTER_SUCCESS,
 	USER_REGISTER_FAIL,
+	USER_DETAILS_REQUEST,
+	USER_DETAILS_SUCCESS,
+	USER_DETAILS_FAIL,
 } from '../constants/userConstants'
 import axios from 'axios'
 
@@ -72,6 +75,33 @@ export const register = (name, email, password) => async (dispatch) => {
 	} catch (error) {
 		dispatch({
 			type: USER_REGISTER_FAIL,
+			payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+		})
+	}
+}
+
+export const getUserDetails = (id, token) => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: USER_DETAILS_REQUEST,
+		})
+
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		}
+
+		const { data } = await axios.get(`/api/users/${id}`, config)
+
+		dispatch({
+			type: USER_DETAILS_SUCCESS,
+			payload: data,
+		})
+	} catch (error) {
+		dispatch({
+			type: USER_DETAILS_FAIL,
 			payload: error.response && error.response.data.message ? error.response.data.message : error.message,
 		})
 	}
